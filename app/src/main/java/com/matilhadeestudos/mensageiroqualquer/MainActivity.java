9f340be9d64bae4704c5e3143e0177ff912b2495
RegroupGit <regroup160@gmail.com>
2022-06-06 23:06:19 -0300

    Teste do repositório

--- app/src/main/java/com/matilhadeestudos/mensageiroqualquer/MainActivity.java
+++ app/src/main/java/com/matilhadeestudos/mensageiroqualquer/MainActivity.java
@@ -12,7 +12,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void pudim() {
-        System.out.println("Pudim de leite condensado");
-    }
 }
