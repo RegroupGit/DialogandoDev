afd8787f5d4e06c830b8037c7c34876d72d801d5
RegroupGit <106414545+RegroupGit@users.noreply.github.com>
2022-06-20 04:17:03 -0300

    splash screen arrumada area de entrada direta para usuarios logados e login para não logados

--- app/src/main/java/com/matilhadeestudos/mensageiroqualquer/Activity/SplashActivity.java
+++ app/src/main/java/com/matilhadeestudos/mensageiroqualquer/Activity/SplashActivity.java
@@ -8,17 +8,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.TimerTask;
 
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.matilhadeestudos.mensageiroqualquer.Model.Usuario;
 import com.matilhadeestudos.mensageiroqualquer.R;
 public class SplashActivity extends AppCompatActivity {
 
 //
+    private FirebaseAuth mAuth;
     TimerTask timerTask;
-
+    Usuario u = new Usuario();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        mAuth = FirebaseAuth.getInstance();
         getSupportActionBar().hide();
 
         timerTask = new TimerTask() {
@@ -39,8 +43,14 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     private void gotoMainActivity() {
-        Intent intent = new Intent(getApplicationContext(), TelaLogin.class);
-        startActivity(intent);
-        finish();
+        if (mAuth.getCurrentUser() == null) {
+            Intent intent = new Intent(getApplicationContext(), TelaLogin.class);
+            startActivity(intent);
+            finish();
+        }else if (mAuth.getCurrentUser() != null){
+            Intent intent = new Intent(getApplicationContext(), ListaContatos.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
